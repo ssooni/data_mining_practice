46eaf3490b16e0056ba814b59262a7d7510cae4a
Son Chan Ho <ssooni526@gmail.com>
2018-12-09 16:25:39 +0900

    워드 클라우드 마스크 적용

--- regex_nlp_kko/mwordcloud.py
+++ regex_nlp_kko/mwordcloud.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import ast
-from wordcloud import WordCloud
-
+import numpy as np
 import matplotlib.pyplot as plt
 
+from PIL import Image
+from wordcloud import WordCloud
 
-def draw_wordcloud(tokens):
-    pass
+
+def get_except_keyword(filename):
+    keyword_list = list()
+    with open(filename, encoding='utf-8') as f:
+        for keyword in f.readlines():
+            keyword_list.append(keyword.strip())
+    print(keyword_list)
+    return keyword_list
 
 
-if __name__ == '__main__':
-    kkma_result = pd.read_csv("./result/noun_token.csv")
+def draw_wordcloud(kkma_result):
+    # List로 되어있는 열을 Row 단위로 분리
     tokens = pd.DataFrame(kkma_result["token"].apply(lambda x: ast.literal_eval(x)).tolist())
 
     tokens["Date"] = kkma_result["Date"]
@@ -21,30 +28,30 @@ if __name__ == '__main__':
 
     tokens = tokens.set_index(["Date", "Speaker", "timetype", "time", "contents"])
     tokens = tokens.T.unstack().dropna().reset_index()
+
     tokens.columns = ["Date", "Person", "time_type", "time", "sntc", "index", "token"]
     print(tokens.head())
 
+    # 빈도수 집계
     summary = tokens.groupby(["token"])["index"].count().reset_index()
     summary = summary.sort_values(["index"], ascending=[False]).reset_index(drop=True)
-    summary = summary[summary["token"] != '이모티콘']
 
-    summary = summary[summary["token"] != '사진']
-    summary = summary[summary["token"] != '동영상']
-    summary = summary[summary["token"] != '오늘']
-    summary = summary[summary["token"] != '내일']
-
+    # 특정 단어 필터링
+    except_keyword = get_except_keyword("./raw_data/except_word.txt")
+    summary = summary[summary["token"].apply(lambda x: x not in except_keyword)]
     summary = summary[summary["token"].apply(lambda x: len(x) > 1)]
 
-    summary = summary.head(100)
-    # freq_80 = summary["index"].quantile(0.95)
-    # summary = summary[summary["index"] < freq_80]
+    # 이미지 Mask 생성
+    denne_mask = np.array(Image.open("./font/denne.png"))
 
-    # summary.plot.line()
-    # plt.show()
-    print(summary.describe())
-
-    print(" ".join(summary["token"]))
-    wc = WordCloud(font_path='./font/NanumBrush.ttf', background_color='white', width=800, height=600).generate(" ".join(summary["token"]))
+    # 워드클라우드 생성
+    wc = WordCloud(font_path='./font/NanumBrush.ttf', background_color='white', mask=denne_mask, width=800, height=600).generate(" ".join(summary["token"]))
     plt.imshow(wc)
     plt.axis("off")
     plt.show()
+
+
+if __name__ == '__main__':
+    kkma_result = pd.read_csv("./result/noun_token_1.csv")
+    draw_wordcloud(kkma_result)
+
--- regex_nlp_kko/word2vector.py
+++ regex_nlp_kko/word2vector.py
@@ -4,21 +4,17 @@ import pandas as pd
 
 
 tokens = pd.read_csv("./result/all_token_1.csv")
-print(len(tokens))
 tokens = tokens[tokens["contents"] != "이모티콘"]
 tokens = tokens[tokens["contents"] != "사진"]
 tokens = tokens[tokens["contents"] != "동영상"]
 tokens = tokens[tokens["contents"].apply(lambda x: 'http' not in x)]
 
-print(len(tokens))
-
 sentence = tokens["token"].apply(lambda x: ast.literal_eval(x)).tolist()
-print(sentence[0:100])
 model = Word2Vec(sentence, min_count=15, iter=10)
 model.init_sims(replace=True)
 
 print("용돈과 관련된 키워드 : ", model.most_similar("용돈"))
-print("지영와 관련된 키워드 : ", model.most_similar("졍이"))
-print("찬호와 관련된 키워드 : ", model.most_similar("쭈니"))
+print("졍이와 관련된 키워드 : ", model.most_similar("졍이"))
+print("쭈니와 관련된 키워드 : ", model.most_similar("쭈니"))
 
 model.save("a.model")
