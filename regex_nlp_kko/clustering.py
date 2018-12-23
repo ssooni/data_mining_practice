581ab57e38f15cbea62042a3324bc1e9da0d7454
Son Chan Ho <ssooni526@gmail.com>
2018-12-23 14:53:10 +0900

    나눔 고딕으로 글꼴 변경

--- regex_nlp_kko/clustering.py
+++ regex_nlp_kko/clustering.py
@@ -4,39 +4,42 @@ from gensim.models import Word2Vec
 import pandas as pd
 import re
 
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
 
-def cluster():
+def cluster(eps, min_sample):
+    # Word Vector를 Load 합니다.
     model = Word2Vec.load("./result/embedding.model")
+
     word_vector = model.wv.vectors
     match_index = model.wv.index2word
-    print(len(match_index))
     model.init_sims(replace=True)
 
     # 두 글자이상 한글인 경우만 사용
     han = re.compile(r"[가-힣]{2,}")
 
-    for min_sample in range(3, 9):
-        dbscan = DBSCAN(eps=0.675, min_samples=min_sample)
+    # DBSCAN 알고리즘 적용
+    dbscan = DBSCAN(eps=eps, min_samples=min_sample)
+    clusters = dbscan.fit_predict(word_vector)
 
-        clusters = dbscan.fit_predict(word_vector)
-        c = pd.DataFrame(clusters, columns=["cluster"], index=match_index)
+    df = pd.DataFrame(clusters, columns=["cluster"], index=match_index).reset_index()
+    df.columns = ["word", "cluster"]
+    print(df.head())
 
-        c = pd.DataFrame(c.reset_index())
-        print(c.T.unstack())
+    # 한글만 필터링 처리
+    df = df[df["word"].apply(lambda x: len(han.findall(x)) > 0)]
 
-        c = c[c["index"].apply(lambda x: len(han.findall(x)) > 0)]
-        c = c[c["cluster"] != -1]
-        print(c.groupby(["cluster"]).count())
+    # 노이즈 포인트 제거
+    df = df[df["cluster"] != -1]
 
-        c.to_excel(pd.ExcelWriter("./result/cluster_eps_%s.xlsx" % min_sample), index=False)
+    print(df.groupby(["cluster"]).count())
+
+    df.to_excel(pd.ExcelWriter("./result/cluster.xlsx"), index=False)
 
 
 def plot():
-    clstr = pd.read_excel(pd.ExcelFile("./result/DBSCAN_eps0.75/cluster_eps_4.xlsx"))
+    clstr = pd.read_excel(pd.ExcelFile("./result/cluster.xlsx"))
     min_cluster = clstr["cluster"].min()
     max_cluster = clstr["cluster"].max()
 
@@ -45,14 +48,14 @@ def plot():
         clstr_index = clstr[clstr["cluster"] == clstr_num].index
         clstr.loc[clstr_index, "value"] = list(range(0, len(clstr_index) * 3, 3))
 
-    print(clstr)
-    font = fm.FontProperties(fname="./font/NanumBrush.ttf", size=15)
+    font = fm.FontProperties(fname="./font/NanumGothic.ttf", size=12)
 
     fig, ax = plt.subplots()
     clstr.plot.scatter(x="cluster", y="value", ax=ax)
-    clstr[["cluster", "value", "index"]].apply(lambda x: ax.text(*x, fontproperties=font), axis=1)
+    clstr[["cluster", "value", "word"]].apply(lambda x: ax.text(*x, fontproperties=font), axis=1)
     plt.show()
 
 
 if __name__ == '__main__':
+    cluster(eps=0.75, min_sample=6)
     plot()
